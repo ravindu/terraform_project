03d6b7d98b271c8967b09d62fd7db6e8f9137584
rsirimanna <rsirimanna@paypal.com>
2019-04-16 10:14:42 +0530

    Terraform AWS setup

--- resources.tf
+++ resources.tf
@@ -7,3 +7,30 @@ resource "aws_vpc" "test_development_network" {
   }
 
 }
+
+resource "aws_subnet" "subnet1" {
+  #cidr_block="${aws_vpc.test_development_network.id}"
+  cidr_block= "${cidrsubnet(aws_vpc.test_development_network.cidr_block, 3, 1)}"
+  vpc_id="${aws_vpc.test_development_network.id}"
+  availability_zone= "us-west-2a"
+}
+
+resource "aws_subnet" "subnet2" {
+  #cidr_block="${aws_vpc.test_development_network.id}"
+  cidr_block= "${cidrsubnet(aws_vpc.test_development_network.cidr_block, 2, 2)}"
+  vpc_id="${aws_vpc.test_development_network.id}"
+  availability_zone= "us-west-2b"
+}
+
+resource "aws_security_group" "subnetsecurity" {
+  vpc_id= "${aws_vpc.test_development_network.id}"
+  ingress{
+    cidr_blocks=[
+      "${aws_vpc.test_development_network.cidr_block}"
+    ]
+    from_port=80
+    to_port=80
+    protocol= "tcp"
+  }
+}
+
